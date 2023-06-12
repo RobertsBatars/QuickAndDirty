b3fc68698ebb90288c5f9cb94459e5373a59a039
Roberts Batars <roberts.batars@gmail.com>
2023-06-12 20:46:20 +0300

    upd

--- Assets/Scripts/CityGenerator.cs
+++ Assets/Scripts/CityGenerator.cs
@@ -6,12 +6,15 @@ public class CityGenerator : MonoBehaviour
 {
     public Vector2 resolution;
     [SerializeField] GameObject startingTile;
+
     private List<List<Tile>> tiles;
+    private RoadTileChooser chooser;
 
     // Start is called before the first frame update
     void Start()
     {
         InitTiles();
+        chooser = GetComponent<RoadTileChooser>();
         PlaceRoadTile((int)resolution.x / 2, (int)resolution.y/2);
     }
 
@@ -39,6 +42,16 @@ public class CityGenerator : MonoBehaviour
     }
     void PlaceRoadTile(int x, int y)
     {
-
+        if (x >= resolution.x || x < 0 || y < 0 || y >= resolution.y)
+        {
+            return;
+        }
+        Tile tile = chooser.ChooseAcceptableRoadTile(tiles[y][x]);
+        Instantiate(tile.tile, new Vector3(x * 10, y * 10, 0), Quaternion.Euler(0, tile.rotation, 0));
+        
+        if (tile.top == 1)
+        {
+            PlaceRoadTile(x, y + 1);
+        }
     }
 }
