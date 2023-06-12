f2506451c8945c68eaf1d65ce36b1bcc91d7d455
Roberts Batars <roberts.batars@gmail.com>
2023-06-12 15:34:27 +0300

    Car now drives

--- Assets/Scripts/WheelController.cs
+++ Assets/Scripts/WheelController.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class WheelController : MonoBehaviour
 {
+    public float acceleration = 500f;
+    public float breakForce = 300f;
+    public float maxTurnAngle = 15f;
+
+    [Space]
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backLeft;
+    [SerializeField] WheelCollider backRight;
+
+    private float currentAcceleration = 0f;
+    private float currentBreakForce = 0f;
+    private float currentTurnAngle = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,30 @@ public class WheelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentBreakForce = breakForce;
+        }
+        else
+        {
+            currentBreakForce = 0;
+        }
+
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+
+        frontRight.brakeTorque = currentBreakForce;
+        frontLeft.brakeTorque = currentBreakForce;
+        backLeft.brakeTorque = currentBreakForce;
+        backRight.brakeTorque = currentBreakForce;
     }
 }
