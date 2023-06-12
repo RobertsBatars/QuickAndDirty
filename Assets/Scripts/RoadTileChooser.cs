8229d35391ecd05c48020f771a525518b4e90e36
Roberts Batars <roberts.batars@gmail.com>
2023-06-12 19:38:32 +0300

    upd

--- Assets/Scripts/RoadTileChooser.cs
+++ Assets/Scripts/RoadTileChooser.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RoadTileChooser : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,14 @@ public class RoadTileChooser : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void GenerateCity()
+    {
+
+    }
+    void PlaceRoadTile()
+    {
+
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    public int top = 0;
+    public int bottom = 0;
+    public int right = 0;
+    public int left = 0;
+    public float rotation = 0;
+    public GameObject tile;
+}
