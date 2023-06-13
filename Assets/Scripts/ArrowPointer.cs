66291600b84f5a0fdac02cf22617478eb0a38393
Roberts Batars <roberts.batars@gmail.com>
2023-06-13 22:09:34 +0300

    Arrow

--- Assets/Scripts/ArrowPointer.cs
+++ Assets/Scripts/ArrowPointer.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ArrowPointer : MonoBehaviour
 {
+    private Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+        {
+            if (GetComponentInParent<PlayerInventoryLogic>().hasPackage)
+            {
+                target = FindObjectOfType<Dropoff>().transform;
+            }
+            else
+            {
+                target = FindObjectOfType<Package>().transform;
+            }
+        }
+        Vector3 dir = (target.position - transform.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, rotation.eulerAngles.y, transform.eulerAngles.z);
     }
 }
